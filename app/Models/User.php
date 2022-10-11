ed0d3f5c6c3035a326a59d7a1df7f4697239cdb1
TomGieben <t.gieben@outlook.com>
2022-10-11 13:39:20 +0200

    character start

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\HtmlString;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,35 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public string $username;
+    public string $texture;
+    public string $selectedItem;
+    public array $inventory;
+    public int $hp;
+
+    public function renderCharacter(string $html = ''): HtmlString {
+
+        $html .= "";
+
+        return new HtmlString($html);
+    }
+
+    public function createCharacter(): bool {
+        try {
+            $this->username = auth()->user()->name;
+
+            return true;
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    public function resetCharacter(): bool {
+        try {
+            return true;
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
 }
