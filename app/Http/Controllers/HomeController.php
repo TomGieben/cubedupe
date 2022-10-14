626c507612d0d4a78bb96cc694027187e756c322
TomGieben <t.gieben@outlook.com>
2022-10-14 13:18:25 +0200

    improvement

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
         $blocks = Block::all();
 
         World::new();
-        
+
         $world = auth()->user()->worlds()->latest()->first();
 
         return view('home', [
--- resources/views/js/character.blade.php
+++ resources/views/js/character.blade.php
@@ -1,56 +1,67 @@
+@php
+
+use App\Models\Block;
+
+@endphp
 <script>
-        document.getElementById("body").addEventListener("keydown", check);
+    document.getElementById("body").addEventListener("keydown", check);
 
-        objImage = document.getElementById("imagechar");
+    objImage = document.getElementById("imagechar");
 
-        var lastTime = 0;
+    var lastTime = 0;
+    var blockWidth = {{ Block::getVar('width') }}
+    var blockHeight = {{ Block::getVar('height') }}
 
-         function check(e) {
-            var key_code = e.which || e.keyCode;
-                switch (key_code) {
-                case 37: //left arrow key
-                    moveLeft();
-                    break;
-                case 39: //right arrow key
-                    moveRight();
-                    break;
-                case 40: //down arrow key
-                    moveDown();
-                    break;
-                case 32: //space bar            
-                    var now = new Date().getTime(); // Time in milliseconds
-                    if (now - lastTime < 1000) {
-                        return;
-                    } else {
-                        lastTime = now;
-                    }
-                    moveUp();
-                    break;
+    function check(e) {
+        var key_code = e.which || e.keyCode;
+        switch (key_code) {
+            case 37: //left arrow key
+                moveLeft();
+                break;
+            case 39: //right arrow key
+                moveRight();
+                break;
+            case 40: //down arrow key
+                moveDown();
+                break;
+            case 32: //space bar
+                var now = new Date().getTime(); // Time in milliseconds
+                if (now - lastTime < 1000) {
+                    return;
+                } else {
+                    lastTime = now;
                 }
-
-                objImage.scrollIntoView({
-                    behavior: 'auto',
-                    block: 'center',
-                    inline: 'center'
-                });
-          }
-
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
+                moveUp();
+                break;
         }
 
-        function moveLeft() {
-            objImage.style.left = parseInt(objImage.style.left) - 20 + "px";
-        }
-        function moveUp() {
-            objImage.style.top = parseInt(objImage.style.top) - 40 + "px";
-            sleep(500).then(() => { objImage.style.top = parseInt(objImage.style.top) + 40 + "px"; });
-        }
-        function moveRight() {
-            objImage.style.left = parseInt(objImage.style.left) + 20 + "px";
-        }
-        function moveDown() {
-            objImage.style.top = parseInt(objImage.style.top) + 20 + "px";
-        }
+        objImage.scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center'
+        });
+    }
 
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function moveLeft() {
+        objImage.style.left = parseInt(objImage.style.left) - blockWidth + "px";
+    }
+
+    function moveUp() {
+        objImage.style.top = parseInt(objImage.style.top) - (blockHeight * 2) + "px";
+        sleep(500).then(() => {
+            objImage.style.top = parseInt(objImage.style.top) + (blockHeight * 2) + "px";
+        });
+    }
+
+    function moveRight() {
+        objImage.style.left = parseInt(objImage.style.left) + blockWidth + "px";
+    }
+
+    function moveDown() {
+        objImage.style.top = parseInt(objImage.style.top) + blockHeight + "px";
+    }
 </script>
