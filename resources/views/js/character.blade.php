b95a7a849da3e333605300ddda5e1b4ca9693597
XaderGator <xandergeurts@hotmail.nl>
2022-10-14 12:58:30 +0200

    char can now jump once a second

--- resources/views/js/character.blade.php
+++ resources/views/js/character.blade.php
@@ -3,20 +3,28 @@
 
         objImage = document.getElementById("imagechar");
 
+        var lastTime = 0;
+
          function check(e) {
             var key_code = e.which || e.keyCode;
                 switch (key_code) {
                 case 37: //left arrow key
                     moveLeft();
                     break;
-                case 38: //Up arrow key
-                    moveUp();
-                    break;
                 case 39: //right arrow key
                     moveRight();
                     break;
                 case 40: //down arrow key
                     moveDown();
+                    break;
+                case 32: //space bar            
+                    var now = new Date().getTime(); // Time in milliseconds
+                    if (now - lastTime < 1000) {
+                        return;
+                    } else {
+                        lastTime = now;
+                    }
+                    moveUp();
                     break;
                 }
 
@@ -27,11 +35,16 @@
                 });
           }
 
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
         function moveLeft() {
             objImage.style.left = parseInt(objImage.style.left) - 20 + "px";
         }
         function moveUp() {
-            objImage.style.top = parseInt(objImage.style.top) - 20 + "px";
+            objImage.style.top = parseInt(objImage.style.top) - 40 + "px";
+            sleep(500).then(() => { objImage.style.top = parseInt(objImage.style.top) + 40 + "px"; });
         }
         function moveRight() {
             objImage.style.left = parseInt(objImage.style.left) + 20 + "px";
