75f4040133ef2bfcef9d49d495a0faf62e02c073
XaderGator <xandergeurts@hotmail.nl>
2022-10-14 13:20:24 +0200

    changes char

--- resources/views/js/character.blade.php
+++ resources/views/js/character.blade.php
@@ -19,7 +19,7 @@
                     break;
                 case 32: //space bar            
                     var now = new Date().getTime(); // Time in milliseconds
-                    if (now - lastTime < 1000) {
+                    if (now - lastTime < 500) {
                         return;
                     } else {
                         lastTime = now;
@@ -27,30 +27,36 @@
                     moveUp();
                     break;
                 }
-
-                objImage.scrollIntoView({
-                    behavior: 'auto',
-                    block: 'center',
-                    inline: 'center'
-                });
           }
 
         function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
         }
 
+        function updatePOV() {
+            objImage.scrollIntoView({
+                behavior: 'auto',
+                block: 'center',
+                inline: 'center'
+            });
+        }
+
         function moveLeft() {
             objImage.style.left = parseInt(objImage.style.left) - 20 + "px";
+            updatePOV()
         }
         function moveUp() {
             objImage.style.top = parseInt(objImage.style.top) - 40 + "px";
-            sleep(500).then(() => { objImage.style.top = parseInt(objImage.style.top) + 40 + "px"; });
+            sleep(300).then(() => { objImage.style.top = parseInt(objImage.style.top) + 40 + "px"; });
+            updatePOV()
         }
         function moveRight() {
             objImage.style.left = parseInt(objImage.style.left) + 20 + "px";
+            updatePOV()
         }
         function moveDown() {
             objImage.style.top = parseInt(objImage.style.top) + 20 + "px";
+            updatePOV()
         }
 
 </script>
